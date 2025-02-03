7954c1f1b3d61e806c373ac231c546436aedce19
DBRI\yuz <yuzh@hhsc.ca>
2025-02-03 16:57:40 -0500

    initial project set up

---
+++ FHIR_IHE_API/FHIR_IHE_API/Controllers/PatientController.cs
@@ -0,0 +1,39 @@
+using Hl7.Fhir.Model;
+using Hl7.Fhir.Rest;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FHIR_IHE_API.Controllers
+{
+    [ApiController]
+    [Route("api/pdq")]
+    public class PatientController : ControllerBase
+    {
+        private readonly FhirClient _client = new("http://localhost:8090/fhir");
+
+        [HttpGet("search")]
+        public IActionResult SearchPatients([FromQuery] string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return BadRequest("Name is required.");
+            }
+
+            var result = _client.Search<Patient>(new string[] {$"name={name}"});
+
+            var patients = result?.Entry
+                .Where(e => e.Resource is Patient)
+                .Select(e => (Patient) e.Resource)
+                .Select(p => new
+                {
+                    Id = p.Id,
+                    Name = p.Name.FirstOrDefault()?.ToString(),
+                    Gender = p.Gender,
+                    BirthDate = p.BirthDate
+
+                }).ToList();
+
+            return Ok(patients);
+
+        }
+    }
+}
