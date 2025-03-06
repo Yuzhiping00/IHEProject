9807dd0b5d7cebef5a265cad786afe501f6c4745
DBRI\yuz <yuzh@hhsc.ca>
2025-03-06 16:58:43 -0500

    added X icon to close the delete modal

--- ihe-fhir-vue/src/components/DeletePatientModal.vue
+++ ihe-fhir-vue/src/components/DeletePatientModal.vue
@@ -5,41 +5,44 @@ const props = defineProps({
     showModal: {
         type: Boolean,
     },
-    
-    selectedFamilyName:{
-        type:String,
+
+    selectedFamilyName: {
+        type: String,
     },
 
-    selectedGivenName:{
-        type:String,  
+    selectedGivenName: {
+        type: String,
     }
 })
 
-const patientName = computed(() =>{
+const patientName = computed(() => {
     return props.selectedFamilyName + ', ' + props.selectedGivenName
 })
 </script>
 
 <template>
-        <!-- Delete Patient Modal -->
-        <v-dialog max-width="45%" height="20%" v-model="props.showModal">
-            <v-card>
-                <v-card-title>
-                    Confirm Patient Deletion
-                </v-card-title>
-                <v-card-text>
-                    Are you sure you want to delete <strong>{{ patientName }}</strong>
-                </v-card-text>
-                <v-card-actions>
-                    <v-btn color="grey" @click="$emit('cancelDelete')">
-                        Cancel
-                    </v-btn>
-                    <v-btn color="red" @click="$emit('confirmDelete')" >
-                        Confirm
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
-        </v-dialog>
+    <!-- Delete Patient Modal -->
+    <v-dialog max-width="45%" height="20%" v-model="props.showModal">
+        <v-card>
+            <v-card-title class="d-flex justify-space-between align-center">
+                <div class="text-h5 ps-2">
+                  Confirm Patient Deletion
+                </div>
+                <v-btn icon="mdi-close" variant="text" @click="$emit('closeModal')"></v-btn>
+            </v-card-title>
+            <v-card-text>
+                Are you sure you want to delete <strong>{{ patientName }}</strong>
+            </v-card-text>
+            <v-card-actions>
+                <v-btn color="grey" @click="$emit('cancelDelete')">
+                    Cancel
+                </v-btn>
+                <v-btn color="red" @click="$emit('confirmDelete')">
+                    Confirm
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
 
 <style scoped></style>
