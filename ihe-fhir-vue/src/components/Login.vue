077fc7397e42a64294f4fd8a91416cb2172917e0
DBRI\yuz <yuzh@hhsc.ca>
2025-07-23 16:51:16 -0400

    create basic  login vue page

--- ihe-fhir-vue/src/components/Login.vue
+++ ihe-fhir-vue/src/components/Login.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { ref, watch} from 'vue'
+import { ref, watch } from 'vue'
 import { useAuthStore } from '@/stores/authStore';
 import accountService from '@/services/core/accountService';
 import { useRouter } from "vue-router"
@@ -37,14 +37,18 @@ const login = async () => {
     }
 }
 
-watch(() => password.value, (newValue) =>{
-    if(newValue) {
+const resetPassword = () => {
+    router.push({ name: "ResetPassword" })
+}
+
+watch(() => password.value, (newValue) => {
+    if (newValue) {
         errorMessage.value = ""
     }
 })
 
-watch(() => email.value, (newValue) =>{
-    if(newValue) {
+watch(() => email.value, (newValue) => {
+    if (newValue) {
         errorMessage.value = ""
     }
 })
@@ -56,8 +60,8 @@ watch(() => email.value, (newValue) =>{
         <v-alert color="#C51162" type="error" border v-if="errorMessage != ''">
             {{ errorMessage }}
         </v-alert>
-        <br/>
-        <v-row>
+        <br />
+        <!-- <v-row>
             <v-col cols="12" md="8" offset-md="2">
                 <v-card>
                     <v-card-title>
@@ -83,50 +87,55 @@ watch(() => email.value, (newValue) =>{
                     </v-card-text>
                 </v-card>
             </v-col>
-        </v-row>
-        <!-- <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="448" rounded="lg">
-            <div class="text-subtitle-1 text-medium-emphasis">Account</div>
+        </v-row> -->
 
-            <v-text-field density="compact" placeholder="Email address" prepend-inner-icon="mdi-email-outline"
-                variant="outlined"></v-text-field>
+        <v-card class="mx-auto pa-12 pb-8" elevation="8" max-width="540" rounded="lg">
+            <div class="text-h6 text-md-h5 text-lg-h6">Account</div>
+            <v-form ref="loginForm">
+                <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+                    Email </div>
+                <v-text-field v-model="email" density="compact" placeholder="example@example.com"
+                    prepend-inner-icon="mdi-email-outline" :rules="[emailRules.required, emailRules.email]"
+                    variant="outlined">
+                </v-text-field>
 
-            <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
-                Password
+                <div class="text-subtitle-1 text-medium-emphasis d-flex align-center justify-space-between">
+                    Password
 
-                <a class="text-caption text-decoration-none text-blue" href="#" rel="noopener noreferrer"
-                    target="_blank">
-                    Forgot login password?</a>
-            </div>
+                    <v-btn class="text-caption text-decoration-none text-yellow" @click="resetPassword" variant="plain">
+                        Forgot password?
+                    </v-btn>
+                </div>
 
-            <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'" :type="visible ? 'text' : 'password'"
-                density="compact" placeholder="Enter your password" prepend-inner-icon="mdi-lock-outline"
-                variant="outlined" @click:append-inner="visible = !visible"></v-text-field>
+                <v-text-field :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+                    :type="visible ? 'text' : 'password'" density="compact" placeholder="Enter your password"
+                    v-model="password" prepend-inner-icon="mdi-lock-outline" name="input-10-2" variant="outlined"
+                    @click:append-inner="visible = !visible" :rules="[passwordRules.required, passwordRules.min]"
+                    hint="At least 8 characters">
+                </v-text-field>
 
-            <v-card class="mb-12" color="surface-variant" variant="tonal">
-                <v-card-text class="text-medium-emphasis text-caption">
-                    Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for three
-                    hours. If you must login now, you can also click "Forgot login password?" below to reset the login
-                    password.
+                <v-card class="mb-5 mt-2" color="surface-variant" variant="tonal">
+                    <v-card-text class="text-medium-emphasis text-caption text-left">
+                        Warning: After 3 consecutive failed login attempts, you account will be temporarily locked for
+                        three hours. If you must login now, you can also click "Forgot password?" below to reset the
+                        login password.
+                    </v-card-text>
+                </v-card>
+
+                <v-btn class="mb-5 v-btn" color="blue" size="medium" variant="tonal" block @click="login">
+                    Log In
+                </v-btn>
+
+                <v-card-text class="text-center">
+                    <v-btn class="text-yellow text-decoration-none" @click="signUp">
+                        Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
+                    </v-btn>
                 </v-card-text>
-            </v-card>
-
-            <v-btn class="mb-8" color="blue" size="large" variant="tonal" block>
-                Log In
-            </v-btn>
-
-            <v-card-text class="text-center">
-                <a class="text-blue text-decoration-none" href="#" rel="noopener noreferrer" target="_blank">
-                    Sign up now <v-icon icon="mdi-chevron-right"></v-icon>
-                </a>
-            </v-card-text>
-        </v-card> -->
+            </v-form>
+        </v-card>
 
     </v-container>
 
 </template>
 
-<style scoped>
-.v-btn {
-    width: 80%;
-}
-</style>
+<style scoped></style>
